ac138927c6cecf0ee94795e4e81057ad8d39367b
victor256sd <victor256@gmail.com>
2025-08-10 14:42:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ if st.session_state.get('authentication_status'):
                 instructions = INSTRUCTION,
                 input = query,
                 model = model,
-                temperature = 0.6,
+                # temperature = 0.6,
                 tools = [{
                             "type": "file_search",
                             "vector_store_ids": [VECTOR_STORE_ID],
