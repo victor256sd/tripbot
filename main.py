d693afaaf087d294578a32056b9ea78f1b5d8789
victor256sd <victor256@gmail.com>
2026-07-09 06:19:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -146,7 +146,7 @@ if st.session_state.get('authentication_status'):
         st.session_state.cleaned_response = cleaned_response
         st.session_state.file_list_str = file_list_str
 
-        Add a small copy icon button
+        # Add a small copy icon button
         copy_button(
             text=cleaned_response + "\n**File(s): **" + file_list_str,
             tooltip="Copy this text",
